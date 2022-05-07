ad06bb04877bf62cff3f5fcd30bfb9c4fd57e5c6
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-05-07 10:37:15 -0500

    Create Camioneta.java

---
+++ src/main/java/vehiculos/Camioneta.java
@@ -0,0 +1,16 @@
+package vehiculos;
+public class Camioneta extends Vehiculo{
+  private boolean volco;
+  static int cantidadCamionetas=0;
+  public Camioneta(String placa,int puertas,String nombre,int precio,int peso, Fabricante fabricante,boolean volco){
+    super(placa,puertas,90,nombre,precio,peso,"4X4",fabricante);
+    this.volco=volco;
+    cantidadCamionetas+=1;
+  }
+  public void setPuestos(boolean volco){
+    this.volco =volco;
+  }
+  public boolean isVolco(){
+    return volco;
+  }
+}
